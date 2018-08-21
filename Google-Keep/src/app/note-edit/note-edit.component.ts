675cc2e75165b1141c675279cec5346a2218c696
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-08-21 15:31:07 +0530

    CRUD updated

--- Google-Keep/src/app/note-edit/note-edit.component.ts
+++ Google-Keep/src/app/note-edit/note-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Note} from '../Note';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Location} from '@angular/common';
-import {NotesService} from '../notes.service';
+import { Note } from '../Note';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { NotesService } from '../notes.service';
+import { FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-note-edit',
@@ -10,34 +11,65 @@ import {NotesService} from '../notes.service';
   styleUrls: ['./note-edit.component.css']
 })
 export class NoteEditComponent implements OnInit {
-  //@Input() note : Note;
-  TempNote : Note;
-  notes : Note[];
+  TempNote: Note;
+  notes: Note[];
   constructor(
-    private noteservice : NotesService,
-    private route :ActivatedRoute,
-    private location :Location,
-    private router: Router
-  ) { }
-  goBack():void{
+    private noteservice: NotesService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router,
+    private fb: FormBuilder
 
+  ) { };
+  EditForm = this.fb.group({
+    Id : [''],
+    Title: [''],
+    Message: [''],
+    Pinned: [''],
+    CheckList: this.fb.array([]),
+    Label: this.fb.array([])
+  });
+  get Label() {
+    return this.EditForm.get('Label') as FormArray;
+  }
+  addLabel() {
+    this.Label.push(this.fb.group({
+      label: ['']
+    }));
+  }
+  get CheckList() {
+    return this.EditForm.get('CheckList') as FormArray;
+  }
+  addCheckList() {
+    this.CheckList.push(this.fb.group({
+      Checklist: [''],
+      IsChecked: ['']
+    }));
+  }
+  goBack(): void {
     this.location.back();
-
   }
   ngOnInit() {
     this.getNote();
   }
-  getNote() : void
-  {
+  getNote(): void {
     const Id = +this.route.snapshot.paramMap.get("id");
-    this.noteservice.getNoteById(Id).subscribe(notes =>{ this.notes = notes.json();console.log(this.notes);});
+    this.noteservice.getNoteById(Id).subscribe(notes => {
+      this.notes = notes.json();
+      console.log(notes.json());
+      notes.json()[0].checkList.forEach(element => {
+       this.addCheckList();
+      });
+      notes.json()[0].label.forEach(element => {
+        this.addLabel();
+       });
+    });
   }
 
-  saveDetails(note : Note, Id : number){
-    console.log(note);
-    // console.log(Id);
-    this.noteservice.put(note, Id).subscribe(result => console.log(result.status));
-    //this.location.back();
+  saveDetails( Id: number) {
+    console.log(Id);
+    console.log(this.EditForm.value as Note);
+    this.noteservice.put(this.EditForm.value as Note, Id).subscribe(result => console.log(result.status));
     this.router.navigate(['']);
   }
 }
